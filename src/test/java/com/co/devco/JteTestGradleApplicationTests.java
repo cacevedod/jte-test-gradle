03ed856e656df6ea46b9addf192f780deb023020
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-08 14:44:01 -0500

    cambio en un test a ver si descarga el código fuente

--- src/test/java/com/co/devco/JteTestGradleApplicationTests.java
+++ src/test/java/com/co/devco/JteTestGradleApplicationTests.java
@@ -1,5 +1,7 @@
 package com.co.devco;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class JteTestGradleApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test1(){
+		assertTrue(true);
 	}
 
 }
